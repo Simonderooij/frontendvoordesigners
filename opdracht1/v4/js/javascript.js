a37de73336f715829c770d3271ad579e432cface
Simonderooij <45428287+Simonderooij@users.noreply.github.com>
2019-05-07 10:56:51 +0200

    Update javascript.js

--- opdracht1/v4/js/javascript.js
+++ opdracht1/v4/js/javascript.js
@@ -1,8 +1,3 @@
-var huisafbeeldingen = document.getElementsByClassName("woning");
-var huisbeschrijving = document.getElementsByClassName("huisbeschrijving");
-//console.log(huisafbeelding);
-console.log(huisbeschrijving);
-
 var huisafbeelding = document.querySelectorAll(".woning");
 var huisbeschrijving = document.querySelectorAll(".huisbeschrijving");
 
