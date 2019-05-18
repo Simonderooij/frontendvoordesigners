8e9d1f4c9fdaaae5422da9634e84f0e35c59f734
Simonderooij <45428287+Simonderooij@users.noreply.github.com>
2019-05-18 15:57:57 +0200

    Delete my.js

--- opdracht2/js/my.js
+++
@@ -1,9 +0,0 @@
-js
-
-
-
-
-
-/*Er is een array met 10 afbeeldingen voor de achtergrond, en een array met 10 afbeeldingen voor de voorgrond (poppetjes).
-voor de achtergrond zijn 2 buttons, links en rechts die door de array heen gaan en de afbeelding laat zien die er bij hoort. Ook zijn er 2 buttons voor de voorgrond poppetjes die door die array heen gaan en het goede poppetje laten zien. Elke keer als je op een knop klikt moet er een transition komen naar de nieuwe afbeelding.
-Ook moet de functie van de knoppen niet alleen met het klikken van de muis lukken. Dus het moet ook kunnen via toetsen op je toetsenbord.*/
